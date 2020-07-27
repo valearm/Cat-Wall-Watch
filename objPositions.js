52e1e2a54ca3b7cc1573d60701680d87fca12e94
valearm <44111119+valearm@users.noreply.github.com>
2020-07-27 09:48:27 +0200

    Add files via upload

---
+++ objPositions.js
@@ -0,0 +1,10 @@
+var clockHand1LocalMatrix = utils.MakeWorld(0.0, 0.0, 0.002, 0.0, 0.0, 0.0, 1.0);
+
+var clockHand2LocalMatrix = utils.MakeWorld(0.0, 0.0, 0.001, 0.0, 0.0, 0.0, 1.0);
+
+var eye1LocalMatrix = utils.MakeWorld(-0.009095, 0.047, 0.018732, 0.0,0.0,0.0,1.0);
+
+var eye2LocalMatrix = utils.MakeWorld(0.007117, 0.047, 0.018971, 0.0,0.0,0.0,1.0);
+
+
+var tailLocalMatrix = utils.MakeWorld(-0.005182, -0.014557, 0.012112, 0.0,0.0,0.0,1.0);
